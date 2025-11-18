cc76de75570ff7fa538deb06147ab74eca7be14f
bhkim <xgame1123@naver.com>
2025-11-18 10:30:14 +0900

    .

--- kobert_hf/src/utils/__init__.py
+++ kobert_hf/src/utils/__init__.py
@@ -1,5 +1,7 @@
 # 유틸리티 함수
 # 2025-11-07, 김병현 작성
-from .utils import is_drive_mounted
+# 2025-11-18, 김병현 수정 - 명시적 import로 변경
+
+from .utils import is_drive_mounted, is_running_in_colab
 
 __all__ = ["is_drive_mounted", "is_running_in_colab"]
